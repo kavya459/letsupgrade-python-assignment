5d94f5dd008cb903fc53585cc6904345486ec1c6
kavya459 <68588017+kavya459@users.noreply.github.com>
2020-07-24 20:59:29 +0530

    Add files via upload

---
+++ Day3 assignment.py
@@ -0,0 +1,23 @@
+Program of the number is prime or not
+
+Print("enter a number")
+num1=input()
+flag1=0
+for each in range (2, num1):
+   If num1%each==0
+        Print("it is prime number")
+        flag1==1
+   Break
+If(flag1==0)
+   print("it is prime number ")
+  
+Program of sum of n numbers
+
+n= 10
+Sum1= 0
+each= 1
+While each<= n:
+    Sum1= sum1+each
+     n= n+1
+Print(" the sum of n numbers is " n)
+
