266a38d3c4798693601bce024a40cf4a9efc07e0
kavya459 <68588017+kavya459@users.noreply.github.com>
2020-07-24 21:26:27 +0530

    Add files via upload

---
+++ Day4assignment.py
@@ -0,0 +1,13 @@
+Finding index of substring
+
+str2= " hello  everyone good morning to all to"
+str2.find("to")
+
+Checking the string is upper or lower 
+n= 5
+each= 1
+While each<=n:
+   Print ("enter a string")
+   str3= input()
+   str3.islower()
+    str3.isupper()
